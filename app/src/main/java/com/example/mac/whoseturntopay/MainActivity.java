ec78a0e4fac1efbd421697f17023ecb1a82ddbf4
Mahmoud Younes <mr.mosad@gmail.com>
2016-06-11 14:39:19 +0400

    Added the "AddMealActivity"

--- app/src/main/java/com/example/mac/whoseturntopay/MainActivity.java
+++ app/src/main/java/com/example/mac/whoseturntopay/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mac.whoseturntopay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MEAL = "com.example.mac.whoseturntopay.MEAL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show() ;
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        //.setAction("Action", null).show();
+                newMeal();
             }
         });
     }
@@ -48,5 +52,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void newMeal() {
+        Intent intent = new Intent(this, AddMealActivity.class);
+        startActivity(intent);
     }
 }
