f675949230573acfbc7421dfca8731551823a117
Mahmoud Younes <mr.mosad@gmail.com>
2016-06-11 16:54:57 +0400

    Fixed few issues and added input validation (except for date)

--- app/src/main/java/com/example/mac/whoseturntopay/AddMealActivity.java
+++ app/src/main/java/com/example/mac/whoseturntopay/AddMealActivity.java
@@ -1,9 +1,17 @@
 package com.example.mac.whoseturntopay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddMealActivity extends AppCompatActivity {
 
@@ -21,5 +29,37 @@ public class AddMealActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        EditText date = (EditText) findViewById(R.id.date_text);
+        date.setText(new StringBuilder()
+                .append(mDay).append("/")
+                // Month is 0 based so add 1
+                .append(mMonth + 1).append("/")
+                .append(mYear));
+    }
+
+    public void AddNewMeal(View view)
+    {
+        EditText restaurant = (EditText)findViewById(R.id.restaurant_text);
+        EditText amount = (EditText)findViewById(R.id.amount_text);
+        EditText date = (EditText)findViewById(R.id.date_text);
+
+        if( restaurant.getText().toString().length() == 0 )
+            restaurant.setError( "Restaurant name is required!" );
+        else if (amount.getText().toString().length() == 0)
+            amount.setError( "Amount paid is required!" );
+        else
+        {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
+
+
--- app/src/main/java/com/example/mac/whoseturntopay/MainActivity.java
+++ app/src/main/java/com/example/mac/whoseturntopay/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mac.whoseturntopay;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,8 +12,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MEAL = "com.example.mac.whoseturntopay.MEAL";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    //public final static String EXTRA_MEAL = "com.example.mac.whoseturntopay.MEAL";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +37,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        //.setAction("Action", null).show();
+                 //       .setAction("Action", null).show();
                 newMeal();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -52,6 +66,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.mac.whoseturntopay/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.mac.whoseturntopay/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 
     public void newMeal() {
